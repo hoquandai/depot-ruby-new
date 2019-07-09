ca965884d9ce2b82d24d6eba04a6c527580bb67a
HoLegolas <hoquandai@gmail.com>
2019-07-09 10:04:37 +0700

    Task G: Check Out! - Atom Feed

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   root 'store#index', as: 'store_index'
   get 'store/index'
   get 'store/about'
-  resources :products
+  
+  resources :products do
+    get :who_bought, on: :member
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
