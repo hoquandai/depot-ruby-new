274f3df882ced4e36873d0d4a211eb5d2cb48b13
HoLegolas <hoquandai@gmail.com>
2019-07-11 13:26:40 +0700

    add cap file

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,52 +1,44 @@
+#---
+# Excerpted from "Agile Web Development with Rails 5",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails5 for more book information.
+#---
 # be sure to change these values
-user = 'HoLegolas'
-domain = '127.0.0.1'
+user = 'lake'
+domain = '192.168.56.10'
 
 # adjust if you are using RVM, remove if you are not
 set :rvm_type, :system
 set :rvm_ruby_string, 'ruby-2.5.1'
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
 
-set :application, "depot"
-set :repo_url, "https://github.com/HoLegolas/depot-ruby.git"
-set :deploy_to, "/home/lake/Documents/ruby/projects/depot/public/"
-#set :repository,  "git@jellly.com:git/jellly.git"
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+# file paths
+set :application, 'depot'
+set :repo_url, "#{user}@#{domain}:git/#{fetch(:application)}.git" 
+set :deploy_to, "/home/#{user}/deploy/#{fetch(:application)}" 
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-set :deploy_via, :copy
-set :ssh_options, {:forward_agent => true}
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+# distribute your applications across servers (the instructions below put them
+# all on the same server, defined above as 'domain', adjust as necessary)
 role :app, domain
 role :web, domain
 role :db, domain
+
+# you might need to set this if you aren't seeing password prompts
+# or are seeing errors like 'no tty present and no askpass program specified'
+#
+# set :pty true
+
+# As Capistrano executes in a non-interactive mode and therefore doesn't cause
+# any of your shell profile scripts to be run, the following might be needed
+# if (for example) you have locally installed gems or applications.  Note:
+# this needs to contain the full values for the variables set, not simply
+# the deltas.
+#
+# set :default_environment, {
+#   'PATH' => '<your paths>:/usr/local/bin:/usr/bin:/bin',
+#   'GEM_PATH' => '<your paths>:/usr/lib/ruby/gems/1.8'
+# }
+#
+# See https://rvm.io/deployment/capistrano#environment for more info.
